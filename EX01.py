76727ac9fa2c34fcf806700d6cf2e58f91dbb2fc
RafMarques-cell <rafael2006marques@gmail.com>
2026-08-11 19:43:09 -0300

    Update EX01.py

--- EX01.py
+++ EX01.py
@@ -1,1 +1,17 @@
 Print("Hello World")
+---------------------------------------------------
+n1 = 5
+n2 = 10
+
+resultado = n1 + n2
+print("resultado da minha adição:", resultado)
+
+resultado = n1 - n2
+print("Resultado da minha subtração:", resultado)
+
+resultado = n1 * n2
+print("Resultado da minha multiplicação:", resultado)
+
+resultado = n1 / n2
+print("Resultado da minha divisão:", resultado)
+
