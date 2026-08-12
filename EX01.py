ea0590cf8c2695a598f84284c74d811b57a58e53
RafMarques-cell <rafael2006marques@gmail.com>
2026-08-11 21:11:03 -0300

    Update EX01.py

--- EX01.py
+++ EX01.py
@@ -14,4 +14,47 @@ print("Resultado da minha multiplicação:", resultado)
 
 resultado = n1 / n2
 print("Resultado da minha divisão:", resultado)
+---------------------------------------------------
+n1 = float(input("Digite o primeiro valor:"))
+n2 = float(input("Digite o segundo valor:"))
 
+resultado = n1 + n2
+print("resultado da minha adição:", resultado)
+
+resultado = n1 - n2
+print("Resultado da minha subtração:", resultado)
+
+resultado = n1 * n2
+print("Resultado da minha multiplicação:", resultado)
+
+resultado = n1 / n2
+print("Resultado da minha divisão:", resultado)
+---------------------------------------------------
+n1 = float(input("Digite a nota:"))
+n2 = float(input("Digite outra nota:"))
+n3 = float(input("Digite a última nota:"))
+
+media = (n1 + n2 +n3) / 3
+print("Média do aluno:", media)
+---------------------------------------------------
+ # Acrescimo de 20%
+valor = float(input("Digite um valor:"))
+
+ # Forma 01
+novo_valor = valor * 1.20
+print("Primeira forma:", novo_valor)
+
+# Forma 02 - regra de 3
+novo_valor = (valor * 120) / 100
+print("regra de 3", novo_valor)
+
+# Desconto de 12%
+novo_valor = valor * o.88
+print("Desconto de 12:", novo_valor)
+
+# Regra de 3
+novo_valor = (valor * 88) / 100
+print("Desconto ( regra de 3):", novo_valor)
+
+# está dando o resultado estranho, acho que fiz algumas cosia errada
+---------------------------------------------------
